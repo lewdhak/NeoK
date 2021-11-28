e071ee34ffa7e936015c62c5c4760ace2062a8c9
LewdHak <94796025+lewdhak@users.noreply.github.com>
2021-11-28 11:29:32 +0100

    💣💣💣💣

---
+++ tools/addons/logo.py
@@ -0,0 +1,15 @@
+# Import modules
+from colorama import Fore
+
+# Logo
+print(rf"""{Fore.MAGENTA}
+
+ ____     ___   ___   __  _ 
+|    \   /  _] /   \ |  |/ ]
+|  _  | /  [_ |     ||  ' / 
+|  |  ||    _]|  O  ||    \ 
+|  |  ||   [_ |     ||     |
+|  |  ||     ||     ||  .  |
+|__|__||_____| \___/ |__|\_|
+          Created by LewdHak
+{Fore.RESET}""")
